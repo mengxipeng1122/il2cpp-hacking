df60d6b64b3dc835854b297ccf16f9214bf6818b
Meng Xipeng <mengxipeng@gmail.com>
2024-06-19 10:06:36 +0800

    com.girls.games.find.differences: add int32 to boughtLevels

--- games/index.com.girls.games.find.differences.ts
+++ games/index.com.girls.games.find.differences.ts
@@ -39,6 +39,7 @@ const dumpGameManager = () =>{
     const gameManager = GameManager.field('Instance').value as Il2Cpp.Object;
     console.log(`gameManager: ${gameManager}`)  
 
+
     showIl2cppInstances(gameManager);
 
     const scriptableData = gameManager.field('scriptableData').value as Il2Cpp.Object;
@@ -51,10 +52,32 @@ const dumpPersistantData = (data:Il2Cpp.Object) =>{
 
     data.field('coinAmount').value = 999999;
     data.field('adsEnabled').value = false;
+
     const completedLevels = (data.field('completedLevels').value as Il2Cpp.Object)
         .method('ToArray').invoke() as Il2Cpp.Array;
     console.log(`completedLevels: ${JSON.stringify(parseInt32Arrray(completedLevels))}`)
+
+    const boughtLevels = data.field('boughtLevels').value as Il2Cpp.Object;
+    console.log(`boughtLevels: ${boughtLevels.method('ToArray').invoke()}`)
     // data.field('nextLevel').value = Il2Cpp.string('59')
+
+    if (0) {
+
+        for (let level = 3; level <= 108; level++) {
+            const SystemInt32 = C("mscorlib", "System.Int32");
+            const systemInt32 = SystemInt32.method('Parse').overload('System.String')
+                .invoke(Il2Cpp.string(level.toString())) as Il2Cpp.Object;
+            console.log(`systemInt32: ${systemInt32}`)
+
+            console.log(`value: ${boughtLevels.field("_size").value}`)
+
+            boughtLevels.method('Add').invoke(systemInt32);
+        }
+    }
+    //add 
+    // (boughtLevels as Il2Cpp.Object).me
+
+    // 
     
 }
 
@@ -68,6 +91,15 @@ const dumpIAPManager = ()=>{
     iapManager.method('BuyCoin100').invoke();
 }
 
+const dumpHintManager = ()=>{
+    const HintManager = C("Assembly-CSharp", "HintManager");
+    const hintManager = HintManager.field('Instance').value as Il2Cpp.Object;
+
+    console.log(`hintManager: ${hintManager}`)
+    showIl2cppInstances(hintManager);
+
+}
+
 const il2cpp_main = ()=>{
 
 
@@ -75,15 +107,20 @@ const il2cpp_main = ()=>{
 
         console.log(`Unity version: ${Il2Cpp.unityVersion}`)
 
+
+        // Il2Cpp.dump('Unity.dump.cs');
+
+        // il2cpp_hook();
+
+        // dumpHintManager();
+
         // dumpProgressManager();
 
         dumpGameManager();
 
         // dumpIAPManager();
 
-        // Il2Cpp.dump('Unity.dump.cs');
 
-        // il2cpp_hook();
 
         // console.log(`dump scenes: ${JSON.stringify(dumpScenes())}`)
 
